a164efa05059edd75e929f0c3dd60f0833f36c51
deepakcse1 <deepak.kumar.ext1@holcim.com>
2026-07-07 16:35:09 +0530

    Liskov Substitution Return Type Rule Implementation

---
+++ SOLID/LSPSignatureReturnTypeRule.java
@@ -0,0 +1,36 @@
+package SOLID;
+
+public class LSPSignatureReturnTypeRule {
+  
+}
+class Organism{ //super Parent
+}
+class Animal extends Organism{ //parent
+}
+class Dog extends Animal{ //chile
+}
+
+class Parent{
+  public Animal getAnimal(){
+    return new Animal();
+  }
+}
+class Child extends Parent{
+  @Override
+  public Animal getAnimal(){
+    return new Dog();
+  }
+
+  // this is allowed
+  // @Override
+  // public Dog getAnimal(){
+  //   return new Dog();
+  // }
+
+  // this is not allowed
+  // @Override
+  // public Organism getAnimal(){
+  //   return new Dog();
+  // }
+}
+
